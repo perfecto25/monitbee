fa9b3ccbc9a05393bb70c1036516cffaed3a1f18
mike.reider <mike.reider@gmail.com>
2023-11-26 23:51:06 -0500

    init

--- src/handlers/collector.cr
+++ src/handlers/collector.cr
@@ -1,45 +1,42 @@
 require "xml"
+require "xml_converter"
+require "json"
+require "uuid"
+
 
 class CollectorHandler < Marten::Handler
   protect_from_forgery false
   def post
     body = request.body
     xml = XML.parse(body)
-    #puts xml
-#    puts xml.first_element_child.not_nil!
-    # data = xml.first_element_child
+    hash = XMLConverter.new(xml).to_h
 
-    
-    # if data
-    #   monit_id = data["id"] # "1" : String?
-    
-    #   #puts typeof(data.children)                       # XML::NodeSet
-    #   data.children.select(&.element?).each do |child| # Select only element children
-    #     puts typeof(child)                               # XML::Node
-    #     puts "child name: #{child.name}"                                  # firstname : String
-    #     puts child.content
-    #     puts "-----------"                               # Jane : String?
-    #   end
+    if hash && hash["monit"]
+      begin
+        h = hash["monit"].to_json
+        json = JSON.parse(h)
+        monit_id = UUID.new(json["id"].to_s)
+        name = json["server"]["localhostname"].to_s
+        monit_version = json["version"].to_s
+        puts monit_id
+        puts typeof(monit_id)
+        puts monit_version
+        puts typeof(monit_version)
+      rescue exception
+        puts exception
+      end
+    end
 
-    # end
 
-    monit_id = xml.xpath_node("//monit/@id")
-    puts monit_id
-    puts xml.xpath_node("//monit/server/uptime")
-    services =  xml.xpath_node("//monit/services")
-    # services.each do |svc|
-    #  puts svc 
-    # end  
-
-    #node.content
-    #puts typeof(data)
-    #if data
-    #  puts typeof(data["id"])
-      # agent = Agent.all.get_or_create!( id: data["id"]) do |new_record|
-      #   new_record.id = data["id"]
-      #   new_record.version = data["version"]
-      # end
-    #end
+    if monit_id
+      agent = Agent.all.get_or_create!(id: monit_id) do |new_record|
+        new_record.id = monit_id
+        new_record.name = name
+        new_record.version = monit_version
+        new_record.state = 1
+      end
+      puts agent
+    end
     respond("ok")
   end
 end
--- src/models/monit.cr
+++ src/models/monit.cr
@@ -1,16 +1,35 @@
 class Agent < Marten::Model
-  field :id, :uuid, primary_key: true, unique: true
+  with_timestamp_fields
+  field :id, :uuid, primary_key: true, unique: true, index: true
   field :name, :string, max_size: 40
   field :version, :string, max_size: 25
+  field :state, :int, primary_key: false, default: 1
+
 end
 
 class Service < Marten::Model
-  field :id, :big_int, primary_key: true, auto: true
-  field :agent, :many_to_one, to: Agent
+  with_timestamp_fields
+  field :id, :big_int, primary_key: true, auto: true, index: true
+  field :agent, :many_to_one, to: Agent, index: true
   field :name, :string, max_size: 40
-end 
+  field :status, :int, primary_key: false, null: true
+  field :state, :int, primary_key: false, null: true
+  field :svc_type, :int, primary_key: false, null: true
+  field :monitor, :int, primary_key: false, null: true
+  field :event, :string, null: true, max_size: 300
+  field :data, :json, null: true
+  db_unique_constraint :agent_name_constraint, field_names: [:agent, :name]
 
-class Alert < Marten::Model 
-  field :id, :big_int, primary_key: true, auto: true 
+end
+
+class Ack < Marten::Model
+  with_timestamp_fields
+  field :id, :big_int, primary_key: true, auto: true, index: true
+  field :service, :one_to_one, to: Service
+  field :state, :bool
+end
+
+class Alert < Marten::Model
+  field :id, :big_int, primary_key: true, auto: true
   field :agent, :many_to_one, to: Agent
 end
